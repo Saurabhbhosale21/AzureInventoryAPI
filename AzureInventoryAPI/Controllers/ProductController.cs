9d5f88d60a69870df72b79c502784d7aa927025e
Saurabhbhosale21 <Saurabhbhosale220@gmail.com>
2026-03-07 20:37:18 +0530

    Add project files.

---
+++ AzureInventoryAPI/Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AzureInventoryAPI.Services;
+using AzureInventoryAPI.Models;
+
+namespace AzureInventoryAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly ProductService _productService;
+
+        public ProductController(ProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            return Ok(_productService.GetProducts());
+        }
+
+        [HttpPost]
+        public IActionResult AddProduct(Product product)
+        {
+            _productService.AddProduct(product);
+            return Ok(product);
+        }
+    }
+}
---
+++ AzureInventoryAPI/Services/ProductService.cs
@@ -0,0 +1,23 @@
+﻿using AzureInventoryAPI.Models;
+
+namespace AzureInventoryAPI.Services
+{
+    public class ProductService
+    {
+        private static List<Product> products = new List<Product>()
+        {
+            new Product { Id = 1, Name = "Laptop", Price = 50000 },
+            new Product { Id = 2, Name = "Mouse", Price = 500 }
+        };
+
+        public List<Product> GetProducts()
+        {
+            return products;
+        }
+
+        public void AddProduct(Product product)
+        {
+            products.Add(product);
+        }
+    }
+}
